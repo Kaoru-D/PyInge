47c0d779a3eb798854b684e82f8713c014722124
Kaoru-D <daniel.rt5d@gmail.com>
2024-07-15 23:26:59 -0500

    sumar valores de lista y sacar longitud de la misma

---
+++ lista.py
@@ -0,0 +1,8 @@
+temp_TK1 = [25.3, 27.8, 23.5, 26.1, 28.4, 24.9]
+
+def TemperaturaPromedio(lista):
+    promedio=sum(lista)/len(lista)
+    print(f"el promedio de temperatura es {promedio}")
+
+
+TemperaturaPromedio(temp_TK1)
